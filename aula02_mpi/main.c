839ef270bfb14615c3ed543b25cacb347aa1f097
PauloRolim <pauloerb@hotmail.com>
2020-10-03 18:26:25 -0300

    primeiro ola mundo MPI

--- aula02_mpi/main.c
+++ aula02_mpi/main.c
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdio.h>
 #include <mpi.h>
 
 int main(void) {
@@ -9,8 +9,7 @@ int main(void) {
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	std::cout << "Proc" << my_rank << " of " << comm_sz " > Hello World!" 
-	<< std::endl;
+	printf("Proc %d of %d > Hello World!\n", my_rank, comm_sz);
 
 	MPI_Finalize();
 	return 0;
