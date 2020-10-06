c398bb396c78d9f27d1a3a996250598d69619dc5
PauloRolim <pauloerb@hotmail.com>
2020-10-06 16:41:32 -0300

    codigo serial completo e testado

--- calculo_pi/main.cpp
+++ calculo_pi/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <random>
+#include <string>
+#include <sys/time.h>
+
+void como_usar(const std::string &programa)
+{
+    std::cout << "Uso do programa " << programa << " tamanho <valor> " << std::endl;
+}
 
 /*função responsável por sortear aleatoriamente pontos dentro da circunferência*/
 double sorteio(double quantidade)
@@ -26,12 +33,45 @@ double sorteio(double quantidade)
 
 int main(int argc, char const *argv[])
 {      
-    double pi;
-    double pontos = sorteio(10000);
+    int tamanho_problema;
+    std::string comando;
 
-    pi = (4.0 * pontos) / 10000; // onde divisor é igual a quantidade de pontos sorteados
+    if (argc == 1)
+    {
+        como_usar(argv[0]);
+        return 1;
+    }
+    
+    comando = argv[1];
+
+    if (comando != "tamanho")
+    {
+        como_usar(argv[0]);
+        return 1;
+    }
+
+    if (argc == 2)
+    {
+        std::cout << "Informe agora o tamanho do problema para calcular PI" << std::endl;
+        std::cin >> tamanho_problema;
+    } else
+    {
+        comando = atoi(argv[2]);
+    }
+       
+    struct timeval start, stop;
+    double tempo_exec;
+    gettimeofday(&start, 0);
+
+    double pi;
+    double pontos = sorteio(tamanho_problema);
+
+    pi = (4.0 * pontos) / tamanho_problema; // onde divisor é igual a quantidade de pontos sorteados
+
+    gettimeofday(&stop, 0);
+    tempo_exec = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
 
     std::cout << "o valor de pi e " << (double)pi << std::endl;
-
+    std::cout << "O tempo de execução foi " << tempo_exec << std::endl;
     return 0;
 }
