7fdd39e0e15b6f53f74cdeae5ba6fb890fa7958f
Paulo Rolim <pauloerb@hotmail.com>
2020-10-06 11:16:18 -0300

    Melhorias no algorítimo

--- calculo_pi/main.cpp
+++ calculo_pi/main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 #include <random>
 
-
-
-
-
-int main (){
-
+/*função responsável por sortear aleatoriamente pontos dentro da circunferência*/
+double sorteio(double quantidade)
+{
     std::random_device rd;
     std::default_random_engine gen(rd());
     std::uniform_real_distribution<> real_dis(0, 1);
     
-    double acertos = 0;
-    double pi;
-    
-   // double Pontos_aleatorios = real_dis(gen);
+    double acertos;
 
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < quantidade; i++)
     {
         double x_real_randomico = real_dis(gen);
         double y_real_randomico = real_dis(gen);
@@ -25,10 +19,17 @@ int main (){
         {
             acertos++;
         }
-        
     }
-    
-    pi = (4.0 * acertos)/ 10000;      // onde 10 é o numero de pontos sorteados
+
+    return acertos;
+}
+
+int main(int argc, char const *argv[])
+{      
+    double pi;
+    double pontos = sorteio(10000);
+
+    pi = (4.0 * pontos) / 10000; // onde divisor é igual a quantidade de pontos sorteados
 
     std::cout << "o valor de pi e " << (double)pi << std::endl;
 
