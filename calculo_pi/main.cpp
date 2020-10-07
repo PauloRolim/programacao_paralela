1fa2cc12d61c5f0b64b901746345fe4d643e019a
PauloRolim <pauloerb@hotmail.com>
2020-10-07 16:33:20 -0300

    Comentários

--- calculo_pi/main.cpp
+++ calculo_pi/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <random>
 #include <string>
 #include <sys/time.h>
 
+//função responsável por mostrar ao usuário como usar o programa
 void como_usar(const std::string &programa)
 {
     std::cout << "Uso do programa " << programa << " tamanho <valor> " << std::endl;
@@ -42,7 +44,7 @@ int main(int argc, char const *argv[])
         return 1;
     }
     
-    comando = argv[1];
+    comando = argv[1];    //testes das entradas fornecidas pelo usuário
 
     if (comando != "tamanho")
     {
@@ -59,19 +61,26 @@ int main(int argc, char const *argv[])
         comando = atoi(argv[2]);
     }
        
-    struct timeval start, stop;
+    struct timeval start, stop; /* código fornecido pelo professor para registrar o tempo de execução */
     double tempo_exec;
-    gettimeofday(&start, 0);
+    gettimeofday(&start, 0);//início do registro do tempo de execução
+    
+    double pi; //variável criada para armazenar o resultado do cálculo
 
-    double pi;
     double pontos = sorteio(tamanho_problema);
 
-    pi = (4.0 * pontos) / tamanho_problema; // onde divisor é igual a quantidade de pontos sorteados
+    /* utilizei o método de Monte Carlo para calcular o valor aproximado de Pi */
+    pi = (4.0 * pontos) / tamanho_problema; // onde divisor é a quantidade de pontos sorteados (x²+y²)
 
-    gettimeofday(&stop, 0);
+    gettimeofday(&stop, 0); //fim do registro do tempo de execução
+
+    //cálculo do tempo de execução
     tempo_exec = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-
+    
+    //exibe na tela o valor aproximado de Pi
     std::cout << "o valor de pi e " << (double)pi << std::endl;
-    std::cout << "O tempo de execução foi " << tempo_exec << std::endl;
+    
+    //exibe na tela o tempo de execução em notação científica
+    std::cout << "O tempo de execução foi " << (std::scientific) << tempo_exec << std::endl; 
     return 0;
 }
