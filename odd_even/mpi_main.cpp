00a10e284f19277b361edaa67cfe32b4f53f889c
PauloRolim <pauloerb@hotmail.com>
2020-10-27 20:17:30 -0300

    Adicionando os comentários

--- odd_even/mpi_main.cpp
+++ odd_even/mpi_main.cpp
@@ -4,13 +4,15 @@
 #include <stdlib.h>
 #include <random>
 
-#define tamanho_problema 32
+#define tamanho_problema 64
 
 int main(int argc, char *argv[]){
 
     int my_rank, comm_sz;
     int vetor_entrada[tamanho_problema];
-    
+    int var_local;
+    int estagio;
+    int i;
     int tamanho_msg;
 
     for (int i = 0; i < tamanho_problema; i++) //Vetor randomico de entrada
@@ -30,10 +32,9 @@ int main(int argc, char *argv[]){
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); //Tamanho do comunicador ou quantos processos inicializados
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); //Número do processo em execução
 
-    tamanho_msg = tamanho_problema / comm_sz;
-  
+    tamanho_msg = tamanho_problema / comm_sz; /*variável responsável por armazenar o tamanho 
+                                                do vetor que será enviado para cada processo*/
     int vetor_local[tamanho_msg];
-
     
     if (my_rank == 0)
     {   
@@ -59,62 +60,49 @@ int main(int argc, char *argv[]){
         
         std::cout << " " << std::endl;
         */
-        int var_local;
-        int estagio;
-        int i;
-
-        for (estagio = 0; estagio < tamanho_msg; estagio++)
+    for (estagio = 0; estagio < tamanho_msg; estagio++)
+    {
+        if (estagio % 2 == 0) // Fase par da ordenação
         {
-            if (estagio % 2 == 0) // Fase par da ordenação
+            for (i = 1; i < tamanho_msg; i+=2)
             {
-                for (i = 1; i < tamanho_msg; i+=2)
+                if (vetor_local[i-1] > vetor_local[i])
                 {
-                    if (vetor_local[i-1] > vetor_local[i])
-                    {
-                        var_local = vetor_local[i];
-                        vetor_local[i] = vetor_local[i - 1];
-                        vetor_local[i - 1] = var_local;
-                    }
-                }         
-            } else // Fase ímpar da ordenação
+                    var_local = vetor_local[i];
+                    vetor_local[i] = vetor_local[i - 1];
+                    vetor_local[i - 1] = var_local;
+                }
+            }          
+        }else // Fase ímpar da ordenação
+        {
+            for (i = 1; i < tamanho_msg - 1; i+=2)
             {
-                for (i = 1; i < tamanho_msg - 1; i+=2)
+                if (vetor_local[i] > vetor_local[i+1])
                 {
-                    if (vetor_local[i] > vetor_local[i+1])
-                    {
-                        var_local = vetor_local[i];
-                        vetor_local[i] = vetor_local[i+1];
-                        vetor_local[i+1] = var_local;
-                    }   
+                    var_local = vetor_local[i];
+                    vetor_local[i] = vetor_local[i+1];
+                    vetor_local[i+1] = var_local;
                 }   
-            }    
-        }
-        /*
+            }   
+        }    
+    }
+    /*
         std::cout << "Processo " << my_rank << " ordenado: ";
 
         for (int i = 0; i < tamanho_msg; i++)
         {
             std::cout << vetor_local[i] << " ";
         }
-        
-      //  MPI_Send(vetor_local, tamanho_msg, MPI_INT, 0, my_rank, MPI_COMM_WORLD);
-  
+          
     std::cout << " " << std::endl;       
-    */
-
-    std::cout << " " << std::endl; 
-
-    MPI_Barrier(MPI_COMM_WORLD);
+    */ 
 
     MPI_Gather(vetor_local,tamanho_msg,MPI_INT,vetor_entrada,tamanho_msg,MPI_INT,0,MPI_COMM_WORLD);
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     if (my_rank == 0)
     {
-
-        int var_local;
-        int estagio;
-        int i;
-
         for (estagio = 0; estagio < tamanho_problema; estagio++)
         {
             if (estagio % 2 == 0) // Fase par da ordenação
