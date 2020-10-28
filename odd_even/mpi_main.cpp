60d8c8ae450974359c6d78d63ef3b0574a58b69f
PauloRolim <pauloerb@hotmail.com>
2020-10-27 21:11:26 -0300

    Add medição do tempo de execução

--- odd_even/mpi_main.cpp
+++ odd_even/mpi_main.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <stdlib.h>
 #include <random>
+#include <sys/time.h>
 
-#define tamanho_problema 64
+#define tamanho_problema 200000
 
 int main(int argc, char *argv[]){
 
@@ -14,6 +15,8 @@ int main(int argc, char *argv[]){
     int estagio;
     int i;
     int tamanho_msg;
+
+    struct timeval start, stop;
 
     for (int i = 0; i < tamanho_problema; i++) //Vetor randomico de entrada
     {
@@ -35,21 +38,23 @@ int main(int argc, char *argv[]){
     tamanho_msg = tamanho_problema / comm_sz; /*variável responsável por armazenar o tamanho 
                                                 do vetor que será enviado para cada processo*/
     int vetor_local[tamanho_msg];
-    
+    /*
     if (my_rank == 0)
     {   
         std::cout << "Vetor INICIAL desordenado: ";
         for (int i = 0; i < tamanho_problema; i++)
         {
-            std::cout << vetor_entrada[i] << " ";
+            std::cout << vetor_entrada[i] << " ";   //Imprime na tela o vetor entrada
         }
-
         std::cout << " " << std::endl;
-    }
-            
-    MPI_Scatter (vetor_entrada, tamanho_msg, MPI_INT, &vetor_local, tamanho_msg, MPI_INT, 0, MPI_COMM_WORLD);
+       
+        
+    }*/
+    // separa o vetor entrada em partes e envia para cada processo
+    gettimeofday(&start, 0);
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Scatter (vetor_entrada, tamanho_msg, MPI_INT, &vetor_local, tamanho_msg, MPI_INT, 0, MPI_COMM_WORLD);
+    
         /*
         std::cout << "Processo " << my_rank << " recebido: ";
 
@@ -96,10 +101,10 @@ int main(int argc, char *argv[]){
           
     std::cout << " " << std::endl;       
     */ 
-
+    //junta as partes de vetores num único vetor e envia para o processo mestre
     MPI_Gather(vetor_local,tamanho_msg,MPI_INT,vetor_entrada,tamanho_msg,MPI_INT,0,MPI_COMM_WORLD);
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD); //sincroniza as mensagens antes de enviar
     
     if (my_rank == 0)
     {
@@ -130,6 +135,8 @@ int main(int argc, char *argv[]){
             }    
         }
 
+        gettimeofday(&stop, 0);
+        /*
         std::cout << "Vetor FINAL ordenado: ";
         for (int i = 0; i < tamanho_problema; i++)
         {
@@ -137,9 +144,23 @@ int main(int argc, char *argv[]){
         }
         
         std::cout << " " << std::endl;
+        */
+        FILE *fp;
+		char outputFilename[] = "tempo_de_exe_mpi.txt";
+
+        fp = fopen(outputFilename, "a");
+		if (fp == NULL) {
+			fprintf(stderr, "Can't open output file %s!\n", outputFilename);
+			exit(1);
+		}
+
+        fprintf(fp, "\t%1.2e ", (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec));
+        fprintf(fp, "\t%d ",comm_sz); //número de tasks mpi
+		fprintf(fp, "\t%d ",tamanho_problema); //tamanho do problema+1
+
+        fclose(fp);
     }
 
     MPI_Finalize();
     return 0;
 }
-
--- odd_even/serial_main.cpp
+++ odd_even/serial_main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stdlib.h>
 #include <random>
+#include <sys/time.h>
 
-#define tamanho_problema 16
+#define tamanho_problema 200000
 
 int ordenacao(int vetor[tamanho_problema]){
 
@@ -9,25 +11,24 @@ int ordenacao(int vetor[tamanho_problema]){
     int estagio;
     int i;
 
-    for (estagio = 0; estagio < tamanho_problema; estagio++)
-    {
-        if (estagio % 2 == 0) // Fase par da ordenação
-        {
-            for (i = 1; i < tamanho_problema; i+=2)
-            {
-                if (vetor[i-1] > vetor[i])
-                {
+    for (estagio = 0; estagio < tamanho_problema; estagio++){
+        if (estagio % 2 == 0){ // Fase par da ordenação
+        
+            for (i = 1; i < tamanho_problema; i+=2){
+            
+                if (vetor[i-1] > vetor[i]){
+                
                     var_local = vetor[i];
                     vetor[i] = vetor[i - 1];
                     vetor[i - 1] = var_local;
                 }
             }         
-        } else // Fase ímpar da ordenação
-        {
-            for (i = 1; i < tamanho_problema - 1; i+=2)
-            {
-                if (vetor[i] > vetor[i+1])
-                {
+        }else{ // Fase ímpar da ordenação
+        
+            for (i = 1; i < tamanho_problema - 1; i+=2){
+            
+                if (vetor[i] > vetor[i+1]){
+                
                     var_local = vetor[i];
                     vetor[i] = vetor[i+1];
                     vetor[i+1] = var_local;
@@ -40,32 +41,54 @@ int ordenacao(int vetor[tamanho_problema]){
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
 
+    struct timeval start, stop;
+    
     int vetor_entrada[tamanho_problema];
         
-    std::cout << "Númermos que serão ordenados: " << std::endl;
+    //std::cout << "Númermos que serão ordenados: " << std::endl;
 
-    for (int i = 0; i < tamanho_problema; i++)
-    {
+    gettimeofday(&start, 0);
+
+    for (int i = 0; i < tamanho_problema; i++){
+
         std::random_device rd;
         std::default_random_engine gen(rd());
         std::uniform_int_distribution<>dis(1,16);
         int valor_aleatorio = std::round(dis(gen));
 
         vetor_entrada[i] = valor_aleatorio;
-        std::cout << vetor_entrada[i] << " ";
+       // std::cout << vetor_entrada[i] << " ";
 
     }
-    std::cout << std::endl;
-    std::cout << "Números após ordenação: "  << std::endl;
+    //std::cout << std::endl;
+    //std::cout << "Números após ordenação: "  << std::endl;
 
     ordenacao(vetor_entrada);
-   
+    /*
     for (int o = 0; o < tamanho_problema; o++)
     {
         std::cout << vetor_entrada[o] << " ";
     }
     std::cout << std::endl;
+    */
+    gettimeofday(&stop, 0);
+
+    FILE *fp;
+		char outputFilename[] = "tempo_de_exe_serial.txt";
+	
+		fp = fopen(outputFilename, "a");
+		if (fp == NULL) {
+			fprintf(stderr, "Can't open output file %s!\n", outputFilename);
+			exit(1);
+		}
+	
+		//testes de impressão no arquivo
+		fprintf(fp, "\t%1.2e ", (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec));
+		fprintf(fp, "\t%d ",tamanho_problema); //tamanho do problema+1
+
+		fclose(fp);
+
     return 0;
 }
